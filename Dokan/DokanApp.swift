0fdd6c4e75879457d79f1700667aeb503aa304cc
JayVnl <jorrit.v@outlook.com>
2022-04-27 15:46:42 +0200

    Initial Commit

---
+++ Dokan/DokanApp.swift
@@ -0,0 +1,17 @@
+//
+//  DokanApp.swift
+//  Dokan
+//
+//  Created by Jorrit Verstappen on 27/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct DokanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
